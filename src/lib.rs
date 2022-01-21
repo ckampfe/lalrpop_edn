839067590bfbf319a0dda051f43b9520cf0303fd
Clark Kampfe <clark.kampfe@gmail.com>
2022-01-21 01:29:57 -0600

    tests: only create parser once per fn

--- src/lib.rs
+++ src/lib.rs
@@ -10,304 +10,206 @@ mod tests {
 
     #[test]
     fn deps() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
         let s = std::fs::read_to_string("deps.edn").unwrap();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, &s)
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, &s).is_ok());
     }
 
     #[test]
     fn strings() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\"\"")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\"hello\"")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\"\"").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\"hello\"").is_ok());
     }
 
     #[test]
     fn symbols() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "hello")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "hello").is_ok());
 
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(&mut keywords_cache, "some_namespace/hello")
             .is_ok());
 
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(&mut keywords_cache, "some_namespace/hello]")
             .is_err());
     }
 
     #[test]
     fn keywords() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, ":hello")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, ":hello").is_ok());
 
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(&mut keywords_cache, ":some_namespace/hello")
             .is_ok());
 
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(&mut keywords_cache, ":some_namespace/hello]")
             .is_err());
     }
 
     #[test]
     fn characters() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\n")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\newline")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\n").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\newline").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\r")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\return")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\r").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\return").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\b")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\backspace")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\b").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\backspace").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\space")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\space").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\t")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\tab")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\t").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\tab").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "\\u241F")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "\\u241F").is_ok());
     }
 
     #[test]
     fn integers() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "42")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "42").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+42")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+42").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+42N")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+42N").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42N")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42N").is_ok());
     }
 
     #[test]
     fn floats() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "0.0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "0.0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+0.0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+0.0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-0.0")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-0.0").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "42.1")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "42.1").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+42.1")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+42.1").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "+42M")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "+42M").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42M")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42M").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1e10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1e10").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1e+10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1e+10").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1e-10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1e-10").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1E10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1E10").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1E+10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1E+10").is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "-42.1E-10")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "-42.1E-10").is_ok());
     }
 
     #[test]
     fn vectors() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[]")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[[]]")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[[] []]")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[[], []]")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[[], [],]")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "[[], [],,,,,]")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[]").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[[]]").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[[] []]").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[[], []]").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[[], [],]").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "[[], [],,,,,]").is_ok());
     }
 
     #[test]
     fn lists() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "()")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "(())")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "(() ())")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "((), ())")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "((), (),)")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "((), (),,,,,)")
-            .is_ok());
+        assert!(parser.parse(&mut keywords_cache, "()").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "(())").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "(() ())").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "((), ())").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "((), (),)").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "((), (),,,,,)").is_ok());
     }
 
     #[test]
     fn sets() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "#{}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "#{#{}}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "#{#{} #{}}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "#{#{}, #{}}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "#{#{}, #{},}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
+        assert!(parser.parse(&mut keywords_cache, "#{}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "#{#{}}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "#{#{} #{}}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "#{#{}, #{}}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "#{#{}, #{},}").is_ok());
+        assert!(parser
             .parse(&mut keywords_cache, "#{#{}, #{},,,,,}")
             .is_ok());
     }
 
     #[test]
     fn maps() {
+        let parser = edn::ExprParser::new();
+
         let mut keywords_cache = std::collections::HashSet::new();
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{[] []}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{:hi :there}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{:hi \"ok\"}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{fine blah}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
+        assert!(parser.parse(&mut keywords_cache, "{}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "{[] []}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "{:hi :there}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "{:hi \"ok\"}").is_ok());
+        assert!(parser.parse(&mut keywords_cache, "{fine blah}").is_ok());
+        assert!(parser
             .parse(&mut keywords_cache, "{[], []          ,}")
             .is_ok());
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{:hi, :there,,}")
-            .is_ok());
-        assert!(edn::ExprParser::new()
+        assert!(parser.parse(&mut keywords_cache, "{:hi, :there,,}").is_ok());
+        assert!(parser
             .parse(&mut keywords_cache, "{:hi,,, ,, \"ok\"}")
             .is_ok());
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(&mut keywords_cache, "{fine ,,,,blah,,,,,}")
             .is_ok());
-        assert!(edn::ExprParser::new()
+        assert!(parser
             .parse(
                 &mut keywords_cache,
                 "{
@@ -321,8 +223,6 @@ mod tests {
             )
             .is_ok());
 
-        assert!(edn::ExprParser::new()
-            .parse(&mut keywords_cache, "{[]}")
-            .is_err());
+        assert!(parser.parse(&mut keywords_cache, "{[]}").is_err());
     }
 }
